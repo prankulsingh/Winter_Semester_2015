cb2a915f384e1a632d326fa117ec4985791c9c34
prankulsingh <prankulsinghps@gmail.com>
2016-03-08 23:54:59 +0530

    initial push...

--- app/src/main/java/com/example/prankul/wintersemester2015/CourseListActivity.java
+++ app/src/main/java/com/example/prankul/wintersemester2015/CourseListActivity.java
@@ -14,6 +14,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 import com.example.prankul.wintersemester2015.dummy.DummyContent;
