fa48816df0cb1ebee56040fa486cf370fadb1dd5
prankulsingh <prankulsinghps@gmail.com>
2016-03-09 00:18:08 +0530

    added sources changed theme

--- app/src/main/java/com/example/prankul/wintersemester2015/dummy/DummyContent.java
+++ app/src/main/java/com/example/prankul/wintersemester2015/dummy/DummyContent.java
@@ -27,13 +27,17 @@ public class DummyContent {
     {
         // Add some sample items.
         addItem(new DummyItem("1","CN - Notes", "http://www.cse.iitk.ac.in/users/dheeraj/cs425/"));
-        addItem(new DummyItem("2","DBMS - Book","http://www.db-book.com/"));
-        addItem(new DummyItem("3","DBMS - Notes","http://www.mathcs.emory.edu/~cheung/Courses/377/Syllabus/syl.html#CURRENT"));
-        addItem(new DummyItem("4","ADA - Resources","https://www.usebackpack.com/iiitd/w2016/cse222/resources"));
-        addItem(new DummyItem("5","GT - Course Overview","https://sites.google.com/a/iiitd.ac.in/graph-theory-w16/"));
-        addItem(new DummyItem("6","GT - Classroom","https://classroom.google.com/u/0/c/NzYyMTA1OTE4"));
-        addItem(new DummyItem("7","EVS - Resources","https://www.usebackpack.com/iiitd/w2016/esc205a/resources"));
-        addItem(new DummyItem("8","T. Comm - Videos","https://www.youtube.com/playlist?list=PLcauF92vqKh-ifba4Vbk4MEcwKuTgngJr"));
+        addItem(new DummyItem("2","DBMS - Resources","https://www.usebackpack.com/iiitd/w2016/cse202/resources"));
+        addItem(new DummyItem("3","DBMS - Book","http://www.db-book.com/"));
+        addItem(new DummyItem("4","DBMS - Notes","http://www.mathcs.emory.edu/~cheung/Courses/377/Syllabus/syl.html#CURRENT"));
+        addItem(new DummyItem("5","ADA - Resources","https://www.usebackpack.com/iiitd/w2016/cse222/resources"));
+        addItem(new DummyItem("6","GT - Course Overview","https://sites.google.com/a/iiitd.ac.in/graph-theory-w16/"));
+        addItem(new DummyItem("7","GT - Classroom","https://classroom.google.com/u/0/c/NzYyMTA1OTE4"));
+        addItem(new DummyItem("8","EVS - Resources","https://www.usebackpack.com/iiitd/w2016/esc205a/resources"));
+        addItem(new DummyItem("9","T. Comm - Videos","https://www.youtube.com/playlist?list=PLcauF92vqKh-ifba4Vbk4MEcwKuTgngJr"));
+        addItem(new DummyItem("10","FooBar", "http://foobar.iiitd.edu.in/contest/team/"));
+        addItem(new DummyItem("11","Backpack", "https://www.usebackpack.com/"));
+        addItem(new DummyItem("12","IIIT Delhi", "https://www.iiitd.ac.in/"));
     }
 
     private static void addItem(DummyItem item) {
